03708f547b62a45af6b415996ff7659de54596b5
pedro bufulin <pedro@semiotic.ai>
2024-03-08 22:10:50 -0300

    test: corrects era validator to use ext_header_from_block

--- tests/era_validator.rs
+++ tests/era_validator.rs
@@ -1,21 +1,21 @@
-use std::env;
-
 use decoder::decode_flat_files;
 use header_accumulator::{
     era_validator::era_validate, errors::EraValidateError, types::ExtHeaderRecord,
-    utils::decode_header_records,
+    utils::ext_header_from_block,
 };
 
 #[test]
 fn test_era_validate() -> Result<(), EraValidateError> {
-    env::set_var("RUST_LOG", "info");
-    env_logger::init();
-
     let mut headers: Vec<ExtHeaderRecord> = Vec::new();
     for number in (0..=8200).step_by(100) {
         let file_name = format!("tests/ethereum_firehose_first_8200/{:010}.dbin", number);
         match decode_flat_files(file_name, None, None) {
-            Ok(blocks) => headers.extend(decode_header_records(blocks).unwrap()),
+            Ok(blocks) => headers.extend(
+                blocks
+                    .iter()
+                    .map(|block| ext_header_from_block(block).unwrap())
+                    .collect::<Vec<ExtHeaderRecord>>(),
+            ),
             Err(e) => {
                 eprintln!("error: {:?}", e);
                 break;
