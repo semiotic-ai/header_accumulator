6b9c6f58aab5c65f462b9e2d1b57cc9fd5613083
pedro bufulin <pedro@semiotic.ai>
2024-03-08 22:15:40 -0300

    test(inclusion_proof): adds tests for failing proofs, proof verification and generation after refactor

--- tests/inclusion_proof.rs
+++ tests/inclusion_proof.rs
@@ -1,23 +1,60 @@
+use decoder::decode_flat_files;
 use header_accumulator::{
     self,
+    errors::EraValidateError,
     inclusion_proof::{generate_inclusion_proof, verify_inclusion_proof},
+    types::ExtHeaderRecord,
+    utils::ext_header_from_block,
 };
+use sf_protos::ethereum::r#type::v2::Block;
 
 #[test]
-fn test_inclusion_proof() {
-    let directory = String::from("tests/ethereum_firehose_first_8200");
+fn test_inclusion_proof() -> Result<(), EraValidateError> {
+    let mut headers: Vec<ExtHeaderRecord> = Vec::new();
+    let mut all_blocks: Vec<Block> = Vec::new(); // Vector to hold all blocks
+
+    for flat_file_number in (0..=8200).step_by(100) {
+        let file_name = format!(
+            "tests/ethereum_firehose_first_8200/{:010}.dbin",
+            flat_file_number
+        );
+        match decode_flat_files(file_name, None, None) {
+            Ok(blocks) => {
+                headers.extend(
+                    blocks
+                        .iter()
+                        .map(|block| ext_header_from_block(block).unwrap())
+                        .collect::<Vec<ExtHeaderRecord>>(),
+                );
+                all_blocks.extend(blocks); // Extend the all_blocks vector with the decoded blocks
+            }
+            Err(e) => {
+                eprintln!("error: {:?}", e);
+                break;
+            }
+        }
+    }
+
     let start_block = 301;
     let end_block = 402;
-    let inclusion_proof = generate_inclusion_proof(&directory, start_block, end_block)
-        .unwrap_or_else(|e| {
+    let inclusion_proof =
+        generate_inclusion_proof(headers, start_block, end_block).unwrap_or_else(|e| {
             println!("Error occurred: {}", e);
             // Handle the error, e.g., by exiting the program or returning a default value
             std::process::exit(1); // Exiting the program, for example
         });
-    assert_eq!(inclusion_proof.len(), end_block - start_block);
+    assert_eq!(
+        inclusion_proof.len() as usize,
+        (end_block - start_block + 1) as usize
+    );
 
     // Verify inclusion proof
-    assert!(
-        verify_inclusion_proof(&directory, None, start_block, end_block, inclusion_proof).is_ok()
-    );
+    let proof_blocks: Vec<Block> = all_blocks[start_block as usize..=end_block as usize].to_vec();
+    assert!(verify_inclusion_proof(proof_blocks, None, inclusion_proof.clone()).is_ok());
+
+    // verify if inclusion proof fails on not proven blocks
+    let proof_blocks: Vec<Block> = all_blocks[302..=403].to_vec();
+    assert!(verify_inclusion_proof(proof_blocks, None, inclusion_proof.clone()).is_err());
+
+    Ok(())
 }
