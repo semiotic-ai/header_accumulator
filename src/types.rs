d5a849d70f65730101523f698aa70abac2640cfd
Joseph Livesey <joseph@semiotic.ai>
2024-07-09 12:10:06 -0400

    fix(types): clippy::unnecessary_fallible_conversions

--- src/types.rs
+++ src/types.rs
@@ -57,8 +57,7 @@ impl TryFrom<&Block> for ExtHeaderRecord {
         Ok(ExtHeaderRecord {
             block_number: block.number,
             block_hash: Hash256::from_slice(&block.hash),
-            total_difficulty: EthereumU256::try_from(total_difficulty.bytes.as_slice())
-                .map_err(|_| EraValidateError::HeaderDecodeError)?,
+            total_difficulty: EthereumU256::from(total_difficulty.bytes.as_slice()),
             full_header: Some(header_from_block(&block)?), // Assuming header_from_block returns Result<_, EraValidateError>
         })
     }
